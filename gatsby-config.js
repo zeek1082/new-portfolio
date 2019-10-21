1331ab698bb6186b106fd6547ffa8d8650c99618
James Smith <james.smith@myriad.com>
2019-10-21 16:51:25 -0500

    Changed bio text

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
   siteMetadata: {
     title: 'James Smith',
     author: 'James Smith',
-    description: 'Hi! I am a Product Design and UX Leader who loves to deliver compelling experiences.',
+    description: 'Hi! I am a Product / UX Designer who loves to deliver compelling experiences.',
     siteUrl: urljoin(siteConfig.url, siteConfig.prefix),
     social: {
       twitter: siteConfig.twitter,
